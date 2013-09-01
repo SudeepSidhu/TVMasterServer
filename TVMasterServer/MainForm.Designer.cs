3a9cebb4fb3a98bc8eefc3c6dad05726fc87ce06
sudeepsidhu <sidhu.sudeep@gmail.com>
2013-09-01 17:21:14 -0500

    Initial commit

---
+++ TVMasterServer/MainForm.Designer.cs
@@ -0,0 +1,174 @@
+﻿namespace TVMasterServer
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lstServers = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnRemove = new System.Windows.Forms.Button();
+            this.txtServerLog = new System.Windows.Forms.TextBox();
+            this.txtDebug = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnClearServerLog = new System.Windows.Forms.Button();
+            this.btnClearDebugLog = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lstServers
+            // 
+            this.lstServers.FormattingEnabled = true;
+            this.lstServers.Location = new System.Drawing.Point(15, 25);
+            this.lstServers.Name = "lstServers";
+            this.lstServers.Size = new System.Drawing.Size(165, 277);
+            this.lstServers.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Servers";
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(15, 308);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 23);
+            this.btnAdd.TabIndex = 2;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnRemove
+            // 
+            this.btnRemove.Location = new System.Drawing.Point(105, 308);
+            this.btnRemove.Name = "btnRemove";
+            this.btnRemove.Size = new System.Drawing.Size(75, 23);
+            this.btnRemove.TabIndex = 3;
+            this.btnRemove.Text = "Remove";
+            this.btnRemove.UseVisualStyleBackColor = true;
+            this.btnRemove.Click += new System.EventHandler(this.btnRemove_Click);
+            // 
+            // txtServerLog
+            // 
+            this.txtServerLog.Location = new System.Drawing.Point(186, 25);
+            this.txtServerLog.Multiline = true;
+            this.txtServerLog.Name = "txtServerLog";
+            this.txtServerLog.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txtServerLog.Size = new System.Drawing.Size(569, 111);
+            this.txtServerLog.TabIndex = 4;
+            // 
+            // txtDebug
+            // 
+            this.txtDebug.Location = new System.Drawing.Point(186, 155);
+            this.txtDebug.Multiline = true;
+            this.txtDebug.Name = "txtDebug";
+            this.txtDebug.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txtDebug.Size = new System.Drawing.Size(569, 147);
+            this.txtDebug.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(183, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(90, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Master Server log";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(183, 139);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(56, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Debug log";
+            // 
+            // btnClearServerLog
+            // 
+            this.btnClearServerLog.Location = new System.Drawing.Point(362, 308);
+            this.btnClearServerLog.Name = "btnClearServerLog";
+            this.btnClearServerLog.Size = new System.Drawing.Size(103, 23);
+            this.btnClearServerLog.TabIndex = 8;
+            this.btnClearServerLog.Text = "Clear Server log";
+            this.btnClearServerLog.UseVisualStyleBackColor = true;
+            this.btnClearServerLog.Click += new System.EventHandler(this.btnClearServerLog_Click);
+            // 
+            // btnClearDebugLog
+            // 
+            this.btnClearDebugLog.Location = new System.Drawing.Point(471, 308);
+            this.btnClearDebugLog.Name = "btnClearDebugLog";
+            this.btnClearDebugLog.Size = new System.Drawing.Size(103, 23);
+            this.btnClearDebugLog.TabIndex = 9;
+            this.btnClearDebugLog.Text = "Clear Debug log";
+            this.btnClearDebugLog.UseVisualStyleBackColor = true;
+            this.btnClearDebugLog.Click += new System.EventHandler(this.btnClearDebugLog_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(761, 336);
+            this.Controls.Add(this.btnClearDebugLog);
+            this.Controls.Add(this.btnClearServerLog);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtDebug);
+            this.Controls.Add(this.txtServerLog);
+            this.Controls.Add(this.btnRemove);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lstServers);
+            this.Name = "MainForm";
+            this.Text = "TV Master Server";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lstServers;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnRemove;
+        private System.Windows.Forms.TextBox txtServerLog;
+        private System.Windows.Forms.TextBox txtDebug;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnClearServerLog;
+        private System.Windows.Forms.Button btnClearDebugLog;
+    }
+}
+
---
+++ TVMasterServer/MainForm.cs
@@ -0,0 +1,329 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace TVMasterServer
+{
+    public partial class MainForm : Form
+    {
+
+        private static UdpClient udpServer;
+        private static TcpListener tcpListener;
+
+        private bool udpON;
+        private bool tcpON;
+        Thread udpThread;
+        Thread tcpThread;
+        
+        private readonly object ioObj = new object();
+        private readonly object logObj = new object();
+        private readonly object listObj = new object();
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            refreshList();
+
+            udpON = true;
+            tcpON = true;
+
+            udpServer = new UdpClient(27900);
+
+            udpThread = new Thread(new ThreadStart(startUDPServer));
+            udpThread.Start();
+
+            tcpThread = new Thread(new ThreadStart(startTCPServer));
+            tcpThread.Start();
+        }
+
+        private void log(String message, bool debugLog)
+        {
+            try
+            {
+                lock (logObj)
+                {
+                    TextBox logBox = txtServerLog;
+
+                    if (debugLog)
+                    {
+                        logBox = txtDebug;
+                    }
+
+                    logBox.Invoke((MethodInvoker)delegate
+                    {
+                        String timeStamp = DateTime.Now.ToShortDateString().ToString() + " " + DateTime.Now.ToLongTimeString().ToString();
+
+                        logBox.Text += timeStamp + ": " + message + "\r\n";
+                    });
+                }
+            }
+            catch (Exception e)
+            {
+                log("In log():" + e.ToString(), true);
+            }
+        }
+
+
+        public void clean()
+        {
+            try
+            {
+                lock (ioObj)
+                {
+                    String strServers = Properties.Settings.Default.Servers;
+                    Array sList = strServers.Split('|');
+
+                    String newServers = "";
+
+                    foreach (String s in sList)
+                    {
+                        if (s.Trim().Length > 0)
+                        {
+                            newServers += s.Trim() + "|";
+                        }
+                    }
+
+                    Properties.Settings.Default.Servers = newServers;
+                    Properties.Settings.Default.Save();
+                }
+            }
+            catch (Exception e)
+            {
+                log("In clean():" + e.ToString(), true);
+            }
+        }
+
+
+        public String getServers()
+        {
+            String strServers = "";
+
+            try
+            {
+                clean();
+
+                lock (ioObj)
+                {
+                    strServers = Properties.Settings.Default.Servers;
+                }
+            }
+            catch (Exception e){
+                log("In getServers():" + e.ToString(), true);
+            }
+
+            return strServers;
+        }
+
+        public void refreshList(){
+
+            try
+            {
+                String strServers = getServers();
+
+                Array sList = strServers.Split('|');
+
+                lock (listObj)
+                {
+                    lstServers.Invoke((MethodInvoker)delegate
+                    {
+                        lstServers.Items.Clear();
+
+                        foreach (String s in sList)
+                        {
+                            if (s.Trim().Length > 0)
+                            {
+                                lstServers.Items.Add(s.Trim());
+                            }
+                        }
+                    });
+                }
+            }
+            catch (Exception e){
+                log("In refreshList():" + e.ToString(), true);
+            }
+        }
+
+        public void addServer(String server)
+        {
+            try{
+                String strServers = getServers();
+
+                lock (ioObj)
+                {
+                    String newServer = server.Trim();
+
+                    if (newServer.Length > 0 && !strServers.Contains(newServer))
+                    {
+                        log("'" + newServer + "' added", false);
+
+                        strServers += newServer + "|";
+                        Properties.Settings.Default.Servers = strServers;
+                        Properties.Settings.Default.Save();
+                    }
+                }
+
+                clean();
+                refreshList();
+            }
+            catch (Exception e)
+            {
+                log("In addServer():" + e.ToString(), true);
+            }
+        }
+
+        public void removeServer(String server)
+        {
+            try
+            {
+                String strServers = getServers();
+
+                lock (ioObj)
+                {
+                    strServers = strServers.Replace(server.Trim(), "");
+                    Properties.Settings.Default.Servers = strServers;
+                    Properties.Settings.Default.Save();
+                    log("'" + server.Trim() + "' removed", false);
+                }
+
+                clean();
+                refreshList();
+            }
+            catch (Exception e)
+            {
+                log("In removeServer():" + e.ToString(), true);
+            }
+        }
+
+        private void startUDPServer()
+        {
+            while (udpON)
+            {
+                try
+                {
+                    IPEndPoint tvServer = new IPEndPoint(IPAddress.Any, 0);
+                    byte[] receiveBytesUDP = udpServer.Receive(ref tvServer);
+
+                    if (receiveBytesUDP != null)
+                    {
+                        String addr = tvServer.Address.ToString();
+                        log("'" + addr + "' just came online", false);
+                        addServer(addr);
+                    }
+                }
+                catch (Exception e){
+                    log("In startUDPServer():" + e.ToString(), true);
+                }
+
+            }
+        }
+
+        private void startTCPServer()
+        {
+            tcpListener = new TcpListener(IPAddress.Any, 27900);
+            tcpListener.Start();
+            ASCIIEncoding asen = new ASCIIEncoding();
+           
+            while (tcpON)
+            {
+                try
+                {
+                    Socket client = tcpListener.AcceptSocket();
+
+                    byte[] b = new byte[100];
+                    int k = client.Receive(b);
+
+                    String message = asen.GetString(b).Replace("\0", "");
+
+                    if (message.StartsWith("serversPlz"))
+                    {
+
+                        log("'" + client.RemoteEndPoint.ToString() + "' is asking for servers", false);
+
+                        String servers = getServers();
+                        client.Send(asen.GetBytes(servers));
+
+                        log("Sent '" + servers + "' to '" + client.RemoteEndPoint.ToString() + "'", true);
+                    }
+
+                    client.Close();
+                }
+                catch (Exception e){
+                    log("In startTCPServer():" + e.ToString(), true);                
+                }
+            }
+        }
+
+        private void btnRemove_Click(object sender, EventArgs e)
+        {
+            if (lstServers.SelectedItems.Count > 0)
+            {
+                for (int i= lstServers.SelectedItems.Count - 1; i >= 0; i--)
+                {
+                    String server = (String)lstServers.SelectedItems[i];
+                    removeServer(server);
+                }
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Environment.Exit(0);
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            int childLeft = (this.Left + this.Width/4);
+            int childTop = (this.Top + this.Height/4);
+
+            String input = Microsoft.VisualBasic.Interaction.InputBox("Enter server's IP address", "Add a server", "", childLeft, childTop).Trim();
+
+            IPAddress address;
+
+            if (IPAddress.TryParse(input, out address))
+            {
+                switch (address.AddressFamily)
+                {
+                    case System.Net.Sockets.AddressFamily.InterNetwork:
+                    case System.Net.Sockets.AddressFamily.InterNetworkV6:
+                        addServer(input);
+                        break;
+                    default:
+                        MessageBoxEx.Show(this, "The address you entered is not a valid IPv4 or IPv6 address", "Whoops", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+                        break;
+                }
+            }
+            else if (input.Length > 0)
+            {
+                MessageBoxEx.Show(this, "The address you entered is not a valid IPv4 or IPv6 address", "Whoops", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+            }
+        }
+
+        private void btnClearServerLog_Click(object sender, EventArgs e)
+        {
+            lock (logObj)
+            {
+                txtServerLog.Clear();
+            }
+        }
+
+        private void btnClearDebugLog_Click(object sender, EventArgs e)
+        {
+            lock (logObj)
+            {
+                txtDebug.Clear();
+            }
+        }
+    }
+}
